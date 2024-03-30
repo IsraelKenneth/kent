145ea6448334453f2c841e737c225281b02b7b4b
vinci-designs <israel.kenneth2011@gmal.com>
2024-03-30 20:45:24 +0100

    update

--- public/asset/Services/index.js
+++
@@ -1,7 +0,0 @@
-import photography from "./web.png"
-import fullstack from "./backend.png"
-import D3 from "./creator.png"
-import GraphicDzn from "./mobile.png"
-
-
-export {photography, fullstack, D3, GraphicDzn}
