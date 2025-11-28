9a436e56b09bea0b3c98b9679b2ab7f201d43863
Kent Dzn <42800016+kent-design1@users.noreply.github.com>
2025-11-28 14:35:10 +0100

    Update index.js

--- components/index.js
+++ components/index.js
@@ -27,6 +27,7 @@ import BentoPrev from "./BentoPre";
 
 import HeroNav from "./extra/HeroNav";
 
-import D3dzn from "./d3dzn";
+import D3dzn from "./D3dzn";
+
 
 export {About,Hero, BentoPrev, D3dzn, Skills,Header2,HeroNav,  Contact, Blog, Barner,  CustomCursor, Header, Navbar, Footer, Works, ZoomParallax, Services}
